87a964620d43e557d3cab24c9317535f8302bb7d
Mohamed Ismail <ismailmdkm@gmail.com>
2022-08-21 17:18:13 +0400

    changes to get login details

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from html.entities import html5
-from flask import render_template
+from flask import redirect, render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -17,3 +18,11 @@ def indes():
     }
     ]
     return render_template('index.html', title="Home", user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requsted for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect('/index')
+    return render_template('login.html', title="Sign In", form=form)
